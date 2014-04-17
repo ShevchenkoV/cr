4fd5d0dadae61d704acbd0b666de8ff980c0c5ee
valerian <valerian.demichev@gmail.com>
2014-04-17 22:37:12 +0300

    Create modules structure

--- app/js/app.js
+++ app/js/app.js
@@ -22,13 +22,13 @@ angular.module('myApp', [
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider
-      .when('/account', {templateUrl: 'partials/account/account.tpl.html', controller: 'AccountCtrl'})
-      .when('/about', {templateUrl: 'partials/about/about.tpl.html', controller: 'AboutCtrl'})
-      .when('/balances', {templateUrl: 'partials/balances/balances.tpl.html', controller: 'BalancesCtrl'})
-      .when('/faq', {templateUrl: 'partials/faq/faq.tpl.html', controller: 'FaqCtrl'})
-      .when('/home', {templateUrl: 'partials/home/home.tpl.html', controller: 'HomeCtrl'})
-      .when('/trade', {templateUrl: 'partials/trade/trade.tpl.html', controller: 'TradeCtrl'})
-      .when('/ticket',{templateUrl:'partials/ticket/ticket.tpl.html',conroller:'TicketCtrl'})
-      .when('/positions',{templateUrl:'partials/positions/positions.tpl.html',conroller:'PositionsCtrl'})
+      .when('/account', {templateUrl: 'js/modules/account/account.tpl.html', controller: 'AccountCtrl'})
+      .when('/about', {templateUrl: 'js/modules/about/about.tpl.html', controller: 'AboutCtrl'})
+      .when('/balances', {templateUrl: 'js/modules/balances/balances.tpl.html', controller: 'BalancesCtrl'})
+      .when('/faq', {templateUrl: 'js/modules/faq/faq.tpl.html', controller: 'FaqCtrl'})
+      .when('/home', {templateUrl: 'js/modules/home/home.tpl.html', controller: 'HomeCtrl'})
+      .when('/trade', {templateUrl: 'js/modules/trade/trade.tpl.html', controller: 'TradeCtrl'})
+      .when('/ticket',{templateUrl:'js/modules/ticket/ticket.tpl.html',conroller:'TicketCtrl'})
+      .when('/positions',{templateUrl:'js/modules/positions/positions.tpl.html',conroller:'PositionsCtrl'})
   .otherwise({redirectTo: '/home'});
 }]);
